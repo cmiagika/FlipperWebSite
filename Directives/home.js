916171398cc866ab86f27fa13946ed11573eec32
Ismail <ismail.kassem@cmiag.ch>
2016-06-03 21:18:03 +0200

    Why this dont work?!

--- Directives/home.js
+++ Directives/home.js
@@ -10,6 +10,15 @@
         var aboutNavigationButton = document.getElementById("aAbout");
         aboutNavigationButton.className = "box";
 
+        var mainContainer = document.getElementById("inhaltContainer");
+        var posCounter;
+        for(posCounter = 2000; posCounter >= 0; posCounter--) {
+           setTimeout(subtractPosition(posCounter), 100);
+        }
+        function subtractPosition(posCounterF) {
+            mainContainer.style.marginLeft = posCounterF + "px";
+            console.log(posCounterF);
+        }
     }
 
     myApp.controller("homeController", Controller);
